d4ffcfe55020e64539719550486b650f1242a79b
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-03-07 09:44:28 +0100

    start export

---
+++ src/builtin_export.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin_export.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: blarger <blarger@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/07 09:34:57 by blarger           #+#    #+#             */
+/*   Updated: 2024/03/07 09:43:03 by blarger          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+/* TO IMPLEMENT
+-> Without any arguments, the command will generate or display all
+	exported variables. 
+
+You can also assign a value before exporting a function as shown
+-> export name[=value]
+-> export students
+ */
+
