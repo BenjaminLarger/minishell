d8e48f52de6846a671153b69abfba9dec8cb1a42
BenjaminLarger <benjaminlarger.bl@gmail.com>
2024-03-13 14:00:10 +0100

    shell process args && utils process

--- src/shell_process_args.c
+++ src/shell_process_args.c
@@ -6,21 +6,43 @@
 /*   By: blarger <blarger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/02 21:44:40 by demre             #+#    #+#             */
-/*   Updated: 2024/03/12 15:43:23 by blarger          ###   ########.fr       */
+/*   Updated: 2024/03/13 13:59:09 by blarger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	process_args_loop(t_minishell *data);
+static void	process_args_loop(t_minishell *data, char **envp);
 
-int	process_args(t_minishell *data)
+/* IMPORTANT
+After using dup2, STDIN refers to the read end of the pipe,
+not the actual standard input.
+When we try to call readline, it tries to perform an operation
+on this new stdin.
+This operation is not supported, leading to an error !!!
+To solve this issue, we can save the initial file descriptor
+using : data->save_stdin_fd = dup(STDIN_FILENO);
+
+The dup() function returns a new file descriptor having the following
+in common with the original open file descriptor fildes:
+	same open file (or pipe)
+	same file pointer (that is, both file descriptors share one file pointer)
+	same access mode (read, write or read/write).
+
+Once the while loop has processed pipes and dup operations, we can restore the
+original STDIN.
+ */
+
+int	process_args(t_minishell *data, char **envp)
 {
 	//split_args_into_cmds(data);
 	//print_all_cmds_and_linkers(data);
+	data->save_stdin_fd = dup(STDIN_FILENO);
 	handle_last_exit_status_cmd(data->args);
 	handle_env_variable(data->args);
-	process_args_loop(data);
+	process_args_loop(data, envp);
+	dup2(data->save_stdin_fd, STDIN_FILENO);
+	close(data->save_stdin_fd);
 	if (data->file.in_fd)
 		close(data->file.in_fd);
 	if (data->file.out_fd)
@@ -30,7 +52,7 @@ int	process_args(t_minishell *data)
 	return (SUCCESS);
 }
 
-static void	process_args_loop(t_minishell *data) //Should we process args one by one or pipe per pipe
+static void	process_args_loop(t_minishell *data, char **envp) //Should we process args one by one or pipe per pipe
 {
 	int	i;
 
@@ -40,14 +62,12 @@ static void	process_args_loop(t_minishell *data) //Should we process args one by
 		if (is_linker(data->args[i]) == TRUE)
 			i += handle_redirection(&(data->args[i]), data);
 		else //it is a simple command
-			i += execute_command(data, i);
-		//}
-		//else => read read-end pipe
+			i += execute_command(data, i, envp);
 		printf("i = %d\n\n", i);
 	}
 }
 
-int	execute_command(t_minishell *data, int i)
+int	execute_command(t_minishell *data, int i, char **envp)
 {
 	if (!ft_strcmp(data->args[i], "echo"))
 		builtin_echo(&(data->args[i]));
@@ -58,8 +78,13 @@ int	execute_command(t_minishell *data, int i)
 	else if (!ft_strcmp(data->args[i], "export"))
 		builtin_export(data->args);
 	else if (!ft_strcmp(data->args[i], "env"))
+	{
 		builtin_env();
-	return count_arg_after_cmd(&(data->args[i]));
+		return (i + 1);
+	}
+	else
+		//simple_command(&(data->args[i]), data, envp);
+	return count_commands(&(data->args[i]));
 	/* else
 		execve() */
 }
--- src/utils_process_args.c
+++ src/utils_process_args.c
@@ -6,7 +6,7 @@
 /*   By: blarger <blarger@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 15:24:35 by blarger           #+#    #+#             */
-/*   Updated: 2024/03/12 12:15:26 by blarger          ###   ########.fr       */
+/*   Updated: 2024/03/13 11:32:49 by blarger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int is_linker(char *str)
 		return (TRUE);
 	return (FALSE);
 }
-int	count_arg_after_cmd(char **args)
+int	count_commands(char **args)
 {
 	int	n;
 
